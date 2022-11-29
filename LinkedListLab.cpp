356c3bd7b6d9c5e09e14715d5a05687de20645f5
nxc10060 <nxc10060@castleton.edu>
2022-11-29 10:42:46 -0500

    lab 13 Nate

--- LinkedListLab.cpp
+++ LinkedListLab.cpp
@@ -12,21 +12,20 @@ struct List {
 
 //Prints the list to the provided output stream
 void print( List* l, std::ostream &out){
-    while ( l != NULL ){
-        //As long as l is not null....
-        out << l->value; //Print out it's value
-        l = l->next; //Move on to the next value
+    if( l == NULL ){
+       return;
+        out << l->next;
+        print( l -> next, out);
     }
 }
 
 //Returns the number of items in the list
 int length(List* l){
-    int length = 0;
-    while ( l != NULL ){
-        l = l->next;
-        length++;
+    if( l == NULL ){
+        return 0;
+    else  
+       return 1 + length(l = l->next);
     }
-    return length;
 }
 
 //Get the Nth item from the list
